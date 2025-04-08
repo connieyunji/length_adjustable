9c5c9553ef3da0c88acaeb4d19d6a591321f6515
connieyunji <142492357+connieyunji@users.noreply.github.com>
2025-04-08 19:42:06 +0900

    Delete 새로운 시도 250408(백업) directory

--- 새로운 시도 250408(백업)/kinematic_250405/calculate_position_1.m
+++
@@ -1,9 +0,0 @@
-function [position_1_x, position_1_y] = calculate_position_1(alpha, l24)
-
-
-position_1 = [l24*cosd(alpha), l24*sind(alpha)];
-position_1_x = position_1(1);
-position_1_y = position_1(2);
-
-end
-
--- 새로운 시도 250408(백업)/kinematic_250405/calculate_position_actuator.m
+++
@@ -1,8 +0,0 @@
-function [position_actuator_x, position_actuator_y] = calculate_position_actuator(position_1_x, position_1_y, l0, a_prime)
-
-
-position_actuator = [position_1_x + l0 * cosd(a_prime) , position_1_y + l0 * sind(a_prime)];
-position_actuator_x = position_actuator(1);
-position_actuator_y = position_actuator(2);
-
-end
